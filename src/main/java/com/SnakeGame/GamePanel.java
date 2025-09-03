8ac35ca75c1f534c4c1e5925c057b528e6a1ec4e
Joshua Sawadda <sawaddajoshua@gmail.com>
2025-09-03 16:07:40 +0200

    Logik für Schlange deklariert und Kopf der Schlange initialisiert

--- src/main/java/com/SnakeGame/GamePanel.java
+++ src/main/java/com/SnakeGame/GamePanel.java
@@ -2,21 +2,37 @@ package com.SnakeGame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class GamePanel extends JPanel {
 
+    // Window preferences
+    private final int TILE_SIZE = 20;
     private final int WIDTH = 800;
     private final int HEIGHT = 600;
 
+    // Game utilities
+    private final ArrayList<Point> snake;
+    private Point head;
+
     public GamePanel() {
+        // Window preferences
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.white);
         setFocusable(true);
+
+        // Game utilities
+        snake = new ArrayList<>();
+        head = new Point(WIDTH / 2, HEIGHT / 2);
+        snake.add(head);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        g.setColor(Color.black);
+        snake.forEach(p -> g.fillRect(p.x, p.y, TILE_SIZE, TILE_SIZE));
     }
 
 }
